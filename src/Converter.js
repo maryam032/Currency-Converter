62c62355834630cd296da679a989a0e7d52513b5
maryam032 <maryamakram032@gmail.com>
2019-07-30 22:38:38 +0500

    Currency converter Task Completed

--- src/Converter.js
+++ src/Converter.js
@@ -46,15 +46,24 @@ class Converter extends React.Component{
         
         return(
             <div>
-                <div style={{marginLeft:'auto',marginRight:'auto',display:'block',marginTop:100,backgroundColor:'yellow',border:'1px solid black',height:80,width:400}}>
-                    <h1>
-                        Currency Converter
-                    </h1>
+                <div style={{marginLeft:'auto',marginRight:'auto',display:'block',color:'white',marginTop:100,backgroundColor:' #FFCA28  ',height:80,width:350}}>
+                <span style={{textAlign:'left',fontSize:25,float:'left',marginLeft:8,display:'block',marginTop:14,fontWeight:'bold'}}>
+                 Currency Converter
+             </span>
+             <img style={{borderRadius:50,display:'block',float:'right',marginTop:0,outline:'none',display:'block',width:"80px" , marginRight:0}}src={process.env.PUBLIC_URL +"/pkr.jpg"}
+                 alt="Mypic" />
+                    
                 </div>
-                    <div style={{marginLeft:'auto',marginRight:'auto',display:'block',backgroundColor:'green',height:400,width:400,marginTop:0,paddingTop:20}}>
-                        <input style={{float:'left',marginRight:'auto',marginLeft:30,display:'block',marginTop:45,borderRadius:10,width:180,height:50,border:'none',outline:'none',paddingLeft:20}} type="text"  onChange={(e)=>{this.handleResult(e,'A')}}  value={this.state.fieldA} placeholder="Enter amount: "  name="fromCurrency"/>
-                        <input style={{float:'left',marginRight:'auto',marginLeft:30,display:'block',marginTop:45,borderRadius:10,width:180,height:50,border:'none',outline:'none',paddingLeft:20}} type="text" onChange={(e)=>{this.handleResult(e,'B')}}  value={this.state.fieldB} placeholder=" answer: "  name="toCurrency"/>
+                    <div style={{marginLeft:'auto',marginRight:'auto',display:'block',backgroundColor:'#AD4532    ',height:250,width:350,marginTop:0,paddingTop:15}}>
+                        <span style={{fontWeight:'bold',display:'block',color:'white',marginLeft:15,float:'left'}}>Converted From</span>
+                        <input style={{display:'block',marginRight:10,float:'right',marginLeft:30,display:'block',marginTop:5,borderRadius:10,width:285,height:40,border:'none',outline:'none',paddingLeft:10}} type="text"  onChange={(e)=>{this.handleResult(e,'A')}}  value={this.state.fieldA} placeholder="PKR "  name="fromCurrency"/>
+                        <span style={{fontWeight:'bold',display:'block',marginLeft:15,float:'left',color:'white',marginTop:10}}>Converted To</span>
+                        <input style={{float:'left',marginRight:56,display:'block',marginTop:5,marginLeft:45,borderRadius:10,width:285,height:45,border:'none',outline:'none',paddingLeft:10}} type="text" onChange={(e)=>{this.handleResult(e,'B')}}  value={this.state.fieldB} placeholder="USD "  name="toCurrency"/>
                         <input  className="Button" type="button" value="Reset" onClick={this.handleReset}/> 
+                        
+
+                       
+
                      </div>
                 <div>
                     <h3>{this.state.result}</h3>
